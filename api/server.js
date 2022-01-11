c0fba4c5e68c0c22168398a46722815fc5cea629
ambrojessica <ambrojessica0@gmail.com>
2022-01-11 01:42:15 -0600

    pushing

--- api/server.js
+++ api/server.js
@@ -1,3 +1,67 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const { restart } = require('nodemon');
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+//
+const server = express();
+server.use(express.json());
+
+//post
+server.post('/api/users', (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    res.status(400).json({
+      message: 'Please provide name and bio for the user',
+    });
+  } else {
+    User.insert(user)
+      .then(createdUser => {
+        restart.status(201).json(createdUser);
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: 'There was an error while saving the user to the database',
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  }
+});
+
+//get
+server.get('/api/users', (req, res) => {
+  User.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The users information could not be retrieved',
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+});
+
+//get id
+server.get('/api/users/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({
+          message: 'The user with the specified ID does not exist'
+        });
+      }
+      res.json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The user information could not be retrieved',
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
