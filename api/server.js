e04c33dedb27c7d94e5a5bfa8d701ef2496fcdfd
ambrojessica <ambrojessica0@gmail.com>
2022-01-11 02:05:04 -0600

    passed tests

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ server.post('/api/users', (req, res) => {
   } else {
     User.insert(user)
       .then(createdUser => {
-        restart.status(201).json(createdUser);
+        res.status(201).json(createdUser);
       })
       .catch(err => {
         res.status(500).json({
@@ -62,6 +62,59 @@ server.get('/api/users/:id', (req, res) => {
         stack: err.stack,
       });
     });
+});
+
+//delete
+server.delete('/api/users/:id', async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res.status(404).json({
+        message: 'The user with the specified ID does not exist'
+      });
+    } else {
+      const deletedUser = await User.remove(possibleUser.id);
+      res.status(200).json(deletedUser);
+    }
+  }
+
+  catch (err) {
+    res.status(500).json({
+      message: 'The user could not be removed',
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
+
+//put
+server.put('/api/users/:id', async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res.status(404).json({
+        message: 'The user with the specified ID does not exist'
+      });
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+          message: 'Please provide name and bio for the user'
+        });
+      } else {
+        const updatedUser = await User.update(
+          req.params.id,
+          req.body
+        );
+        res.status(200).json(updatedUser);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'The user information could not be modified',
+      err: err.message,
+      stack: err.stack,
+    });
+  }
 });
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
